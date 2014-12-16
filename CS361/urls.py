da90263c75555a056abb1c8d928462f2ffa56024
aliuzun <aliuzun@std.sehir.edu.tr>
2014-12-16 10:55:43 +0200

    Soem html file added,database get better

--- CS361/urls.py
+++ CS361/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+import myfile.views as ms
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'CS361.views.home', name='home'),
+    # url(r'^$', 'CS361.myfile.views', name='home'),
+    url(r'^$', ms.index),
+    url(r'base/$', ms.about),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- myfile/admin.py
+++ myfile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from myfile.models import AddActivity,EntryAdmin
 
 # Register your models here.
+
+# admin.site.register(AddActivity)
+# admin.site.register(Product)
+# admin.site.register(EntryAdmin)
